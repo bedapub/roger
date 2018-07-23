c537a92495754197262dc7571db4a6e333cd7fd4
Rudolf Biczok <rudolf.biczok@roche.com>
2018-07-23 17:29:27 +0200

    Add custom SQLAlchemy column type to enable JSON management for databases which don't support json natively

---
+++ roger/persistence/json_backport.py
@@ -0,0 +1,23 @@
+import sqlalchemy.types as types
+import json
+
+
+class StringEncodedJSON(types.TypeDecorator):
+    """Stores and retrieves JSON as TEXT."""
+
+    impl = types.TEXT
+
+    def process_bind_param(self, value, dialect):
+        if value is not None:
+            value = json.dumps(value)
+        return value
+
+    def process_result_value(self, value, dialect):
+        if value is not None:
+            value = json.loads(value)
+        return value
+
+
+# TypeEngine.with_variant says "use StringyJSON instead when
+# connecting to 'sqlite'"
+RogerJSON = types.JSON().with_variant(StringEncodedJSON, 'sqlite')
