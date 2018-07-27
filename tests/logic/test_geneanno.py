f3441fcf19c3fbaf62262b76b387e5d04cf34abd
Rudolf Biczok <rudolf.biczok@roche.com>
2018-07-27 14:32:32 +0200

    Separate single test into two separate ones and update expected data set to new Ensembl version 93

--- tests/logic/test_geneanno.py
+++ tests/logic/test_geneanno.py
@@ -1,3 +1,6 @@
+from pandas.util.testing import assert_frame_equal
+from pandas import read_csv
+
 from tests import app, mouse_tax_id, mouse_dataset
 from tests import db
 
@@ -6,38 +9,49 @@ import roger.logic
 import roger.logic.mart
 import roger.logic.geneanno
 import roger.persistence.geneanno
-from pandas.util.testing import assert_frame_equal
-from pandas import read_csv
 
 
-def test_annotate():
-    with app.app_context():
-        db.create_all()
+class TestAnnotate(object):
 
-        session = db.session()
-        roger.persistence.geneanno.add_species(session,
-                                               roger.persistence.geneanno.human_dataset,
-                                               roger.persistence.geneanno.human_tax_id)
-        roger.persistence.geneanno.add_species(session,
-                                               mouse_dataset,
-                                               mouse_tax_id)
+    def test_annotate_chip_data(self):
+        with app.app_context():
+            db.create_all()
 
-        gct_data = parse_gct(file_path="test_data/ds/ma-example-signals.gct")
-        (feature_data, annotation_version) = roger.logic.geneanno.annotate(session,
-                                                                           gct_data,
-                                                                           mouse_tax_id,
-                                                                           "affy_mouse430_2")
-        assert "Mouse genes" in annotation_version
-        assert_frame_equal(read_csv("test_data/ds/ma-example-rogerFeatureAnno.txt", sep="\t", index_col=0),
-                           feature_data)
+            session = db.session()
+            roger.persistence.geneanno.add_species(session,
+                                                   roger.persistence.geneanno.human_dataset,
+                                                   roger.persistence.geneanno.human_tax_id)
+            roger.persistence.geneanno.add_species(session,
+                                                   mouse_dataset,
+                                                   mouse_tax_id)
 
-        gct_data = parse_gct(file_path="test_data/ds/rnaseq-example-readCounts.gct")
-        (feature_data, annotation_version) = roger.logic.geneanno.annotate(session,
-                                                                           gct_data,
-                                                                           roger.persistence.geneanno.human_tax_id,
-                                                                           "entrezgene")
-        assert "Human genes" in annotation_version
-        assert_frame_equal(read_csv("test_data/ds/rnaseq-example-rogerFeatureAnno.txt", sep="\t", index_col=0),
-                           feature_data)
+            gct_data = parse_gct(file_path="test_data/ds/ma-example-signals.gct")
+            (feature_data, annotation_version) = roger.logic.geneanno.annotate(session,
+                                                                               gct_data,
+                                                                               mouse_tax_id,
+                                                                               "affy_mouse430_2")
+            assert "Mouse genes" in annotation_version
+            assert_frame_equal(read_csv("test_data/ds/ma-example-rogerFeatureAnno.txt", sep="\t", index_col=0),
+                               feature_data)
+            db.drop_all()
 
-        db.drop_all()
+    def test_annotate_entrezgene(self):
+        with app.app_context():
+            db.create_all()
+
+            session = db.session()
+            roger.persistence.geneanno.add_species(session,
+                                                   roger.persistence.geneanno.human_dataset,
+                                                   roger.persistence.geneanno.human_tax_id)
+
+            gct_data = parse_gct(file_path="test_data/ds/rnaseq-example-readCounts.gct")
+            (feature_data, annotation_version) = roger.logic.geneanno.annotate(session,
+                                                                               gct_data,
+                                                                               roger.persistence.geneanno.human_tax_id,
+                                                                               "entrezgene")
+
+            assert "Human genes" in annotation_version
+            assert_frame_equal(read_csv("test_data/ds/rnaseq-example-rogerFeatureAnno.txt", sep="\t", index_col=0),
+                               feature_data)
+
+            db.drop_all()
