bad8495404e45e589b6fd75e1d2fe4590614deec
Rudolf Biczok <rudolf.biczok@roche.com>
2018-08-17 14:04:42 +0200

    Remove unused code block

--- roger/cli/dge.py
+++ roger/cli/dge.py
@@ -20,34 +20,6 @@ def list_dge_methods():
     print(roger.persistence.dge.list_methods(db.session()))
 
 
-# TODO: No method customization for now
-# @cli.command(name="add-dge-method",
-#              short_help='Adds a new Differential Gene Expression method to the database')
-# @click.argument('name', metavar='<name>')
-# @click.argument('description', metavar='<description>')
-# @click.argument('version', metavar='<version>')
-# def add_dge_method(name, description, version):
-#     print("Adding DGE method '%s' ..." % name)
-#     from roger.persistence import db
-#     import roger.persistence.dge
-#
-#     roger.persistence.dge.add_method(db.session(), name, description, version)
-#     print("Done")
-
-
-# TODO: No method customization for now
-# @cli.command(name="remove-dge-method",
-#              short_help='Removes the Differential Gene Expression method with the given name')
-# @click.argument('name', metavar='<name>')
-# def remove_dge_method(name):
-#     print("Deleting DGE method '%s' ..." % name)
-#     from roger.persistence import db
-#     import roger.persistence.dge
-#
-#     roger.persistence.dge.delete_method(db.session(), name)
-#     print("Done")
-
-
 # ---------------
 # Datasets
 # ---------------
