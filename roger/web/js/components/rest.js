f3e1a51b2d367d427843ace3d06734cd33e9d6ee
Rudolf Biczok <rudolf.biczok@roche.com>
2018-10-25 17:58:53 +0200

    Restructure source code into different modules and sites

---
+++ roger/web/js/components/rest.js
@@ -0,0 +1,3 @@
+import React from "react";
+
+export const URL_PREFIX = "http://rkalbhpc002.kau.roche.com:5000/api";
--- roger/web/js/views/index.jsx
+++ roger/web/js/views/index.jsx
@@ -1,11 +1,58 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import ItemLister from "../components/itemlist.jsx";
+import {URL_PREFIX} from "../components/rest";
+
+class StudyList extends React.Component {
+    constructor() {
+        super();
+        this.state = { items: [] };
+    }
+
+    componentDidMount() {
+        fetch(`${URL_PREFIX}/study`)
+            .then(result=>result.json())
+            .then(items=> {
+                this.setState({items: items});
+            });
+    }
+
+    render() {
+        return(
+            <div>
+                {this.state.items.map(item => (
+                    <div key={item.Name} className="study_overview">
+                        <p><span>Name:</span> {item.Name} <Link to='/href' >ASD</Link> </p>
+                        <ul>
+                            <li>
+                                <span>Expression type:</span> {item.ExpressionType}
+                            </li>
+                            <li>
+                                <span>Description:</span> {item.Description}
+                            </li>
+                            <li>
+                                <span>Sample count:</span> {item.SampleCount}
+                            </li>
+                            <li>
+                                <span>Feature count:</span> {item.FeatureCount}
+                            </li>
+                            <li>
+                                <span>Gene annotation version:</span> {item.GeneAnnotationVersion}
+                            </li>
+                            <li>
+                                <span>Created by :</span> {item.CreatedBy}
+                            </li>
+                        </ul>
+                    </div>
+                ))}
+            </div>
+        );
+    }
+}
 
 class IndexView extends React.Component {
     render() {
         return (
-            <div><ItemLister /></div>
+            <div><StudyList /></div>
         );
     }
 }
