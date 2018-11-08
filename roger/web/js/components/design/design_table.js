1a4144c98b5cb0d244368da6e23991aa9454c9d0
Rudolf Biczok <rudolf.biczok@roche.com>
2018-11-08 10:52:23 +0100

    Put table content into a Card component instead of a Paper

--- roger/web/js/components/design/design_table.js
+++ roger/web/js/components/design/design_table.js
@@ -7,61 +7,72 @@ import TableCell from '@material-ui/core/TableCell';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import Typography from '@material-ui/core/Typography';
 
-const styles = theme => ({
-    root: {
-        width: '100%',
-        marginTop: theme.spacing.unit * 3,
-        overflowX: 'auto',
+const styles = {
+    card: {
+        minWidth: 275,
+        marginBottom: 12,
     },
     table: {
         minWidth: 700,
     },
-});
+};
 
 
 function DesignTable(props) {
     const {classes, design, sampleAnnotation} = props;
 
     return (
-        <Paper className={classes.root}>
-            <Table className={classes.table}>
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Sample Group</TableCell>
-                        <TableCell>Used?</TableCell>
-                        <TableCell numeric>Sample Name</TableCell>
-                        {design.DesignMatrix.map(col => {
-                            return <TableCell key={col.columnName} numeric>
-                                {col.columnName} {col.isCovariate ? <span><br/>(covariate)</span> : ""}
-                            </TableCell>
-                        })}
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {sampleAnnotation.data.map((sample, index) => {
-                        return (
-                            <TableRow key={sample.SAMPLE}>
-                                <TableCell component="th" scope="row" numeric>
-                                    {design.SampleGroups[index]}
+        <Card className={classes.card}>
+            <CardContent>
+                <Typography variant="h5" component="h2">
+                    Design Matrix & Sample Information
+                </Typography>
+                <Table className={classes.table}>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell colSpan={3}>Sample Information</TableCell>
+                            <TableCell colSpan={design.VariableCount}>Variables</TableCell>
+                        </TableRow>
+                        <TableRow>
+                            <TableCell numeric>Sample Group</TableCell>
+                            <TableCell numeric>Used?</TableCell>
+                            <TableCell numeric>Sample Name</TableCell>
+                            {design.DesignMatrix.map(col => {
+                                return <TableCell key={col.columnName} numeric>
+                                    {col.columnName} {col.isCovariate ? <span><br/>(covariate)</span> : ""}
                                 </TableCell>
-                                <TableCell component="th" scope="row" numeric>
-                                    {design.SampleSubset[index].IsUsed ? "Yes" : "No"}
-                                </TableCell>
-                                <TableCell component="th" scope="row" numeric>
-                                    {sample.SAMPLE}
-                                </TableCell>
-                                {design.DesignMatrix.map(col => {
-                                    return <TableCell key={col.columnName} numeric>
-                                        {col.values[index]}
+                            })}
+                        </TableRow>
+                    </TableHead>
+                    <TableBody>
+                        {sampleAnnotation.data.map((sample, index) => {
+                            return (
+                                <TableRow key={sample.SAMPLE}>
+                                    <TableCell component="th" scope="row" numeric>
+                                        {design.SampleGroups[index]}
+                                    </TableCell>
+                                    <TableCell component="th" scope="row" numeric>
+                                        {design.SampleSubset[index].IsUsed ? "Yes" : "No"}
+                                    </TableCell>
+                                    <TableCell component="th" scope="row" numeric>
+                                        {sample.SAMPLE}
+                                    </TableCell>
+                                    {design.DesignMatrix.map(col => {
+                                        return <TableCell key={col.columnName} numeric>
+                                            {col.values[index]}
                                         </TableCell>
-                                })}
-                            </TableRow>
-                        );
-                    })}
-                </TableBody>
-            </Table>
-        </Paper>
+                                    })}
+                                </TableRow>
+                            );
+                        })}
+                    </TableBody>
+                </Table>
+            </CardContent>
+        </Card>
     );
 }
 
