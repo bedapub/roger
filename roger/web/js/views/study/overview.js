1c989a432fff495c7fbf7fc5b8f6af0353247dff
Rudolf Biczok <rudolf.biczok@roche.com>
2018-11-08 10:36:25 +0100

    Just add spaces

--- roger/web/js/views/study/overview.js
+++ roger/web/js/views/study/overview.js
@@ -8,6 +8,7 @@ import Card from '@material-ui/core/Card';
 import Grid from '@material-ui/core/Grid';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
+
 import StudyDrawer from "../../components/study/study_drawer";
 
 const styles = {
