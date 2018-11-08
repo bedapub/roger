8e5e585ce06c007bb66fc4546d9930be0cce4782
Rudolf Biczok <rudolf.biczok@roche.com>
2018-11-08 11:16:37 +0100

    Reorder imports

--- roger/web/js/views/study/design/overview.js
+++ roger/web/js/views/study/design/overview.js
@@ -4,8 +4,8 @@ import {withStyles} from '@material-ui/core/styles';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
 import ListItemText from '@material-ui/core/ListItemText';
-import Card from '@material-ui/core/Card';
 import Grid from '@material-ui/core/Grid';
+import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import Typography from '@material-ui/core/Typography';
 
